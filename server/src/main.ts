32532ebf1ff97cd6e9e5adde9a2744aa51d8f51c
Prefect-m <mirlan.zaiyrbekov@gmail.com>
2023-07-09 20:30:31 +0600

    delete old structure

--- server/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-
-const PORT = process.env.PORT || 4100
-const start = async () => {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(PORT)
-}
-start()
--- server/src/users/models/user.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface UserI {
-  id?: number
-  name: string
-  username: string
-  email: string
-  password?: string
-}
